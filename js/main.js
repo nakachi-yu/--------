e6205c5a623967174b32dd55578491ebe3cb5dc8
YU-N <3154docokani@gmail.com>
2026-06-07 02:54:31 +0900

    進めた

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+
+
+  $('.slideshoe').slick({
+  dots: true,
+  arrows: false,      // 矢印非表示
+  infinite: true,
+  speed: 500,
+  fade: true,
+  
+  autoplay: true,
+  autoplaySpeed: 4000,
+  pauseOnHover: false // マウス乗せても止まらない
+  });
+});
+
