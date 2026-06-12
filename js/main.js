bf61fa8395e14f2d0d5d9eb52746388a705e0d5b
YU-N <3154docokani@gmail.com>
2026-06-12 16:42:50 +0900

    fvのh1の動き完了

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 
 $(document).ready(function(){
 
+
+// console.log('動いてる');
+
   var duration = 300;
 
   $('.slideshoe').slick({
@@ -14,17 +17,6 @@ $(document).ready(function(){
   autoplaySpeed: 4000,
   pauseOnHover: false // マウス乗せても止まらない
   });
-
-// $('#buttons1 button:nth-child(n+9)')
-//         .on('mouseover', function(){
-//             $(this).find('> span')
-//                 .stop(true).animate({width: '100%'}, duration, 'easeOutQuad');
-//         })
-//         .on('mouseout', function(){
-//             $(this).find('> span')
-//                 .stop(true).animate({width: '0%'}, duration, 'easeOutQuad');
-//         });
-        
 
 
 
@@ -51,20 +43,65 @@ $('.contact-btn')
 
 
 
+       
+$('.fv-detail h1 span').animate({
+  left: '0%'
+},1000);
+  
+
+// $('.fv-detail h1 span').css({
+//   color:'red',
+//   left:'0',
+//   position:'relative'
+// });
 
 
 
 
+// $('.fv-detail h1 span').animate({
+//   color:'red',
+//   left:'0',
+  
+// });
 
 
 
 
+// $('.fv-detail h1 span').css('left')
+// $(window).on('load', function () {
+//   $('.fv-detail h1 span').animate({
+//     left:'0'
+//   },1000);
+  
+// });
 
 
 
+// console.log($('.fv-detail h1 span').length);
+// setTimeout(function(){
+//   $('.fv-detail h1 span').animate({
+//     left:'0'
+//   },1000);
+// },1000);
 
 
 
+// $('.ilst').css('opactity',0.5);
+
+
+
+$('.fv-detail h1 span').eq(0).css({
+  opacity: 1,
+  transform: 'translateX(0px)'
+});
+
+
+setTimeout(function(){
+$('.fv-detail h1 span').eq(1).css({
+  opacity: 1,
+  transform: 'translateX(0px)'
+});
+},500);
 
 
 
